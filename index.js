c98105269b24f91271b97943ab68b9bff1aa22c4
lijun <lijun401338@qq.com>
2020-04-28 19:33:10 +0800

    add loadable/babel

--- index.js
+++ index.js
@@ -3,176 +3,148 @@ import { matchPath } from 'react-router';
 
 const PAGE_LOADERS = [];
 
-function isModuleLoaded(moduleId) {
-    if (typeof __webpack_modules__ !== "object") {
-        return false;
-    }
-    return (
-        typeof moduleId !== "undefined" &&
-        typeof __webpack_modules__[moduleId] !== "undefined"
-    );
-}
-
-
 function resolveModule(obj) {
     return obj && obj.__esModule ? obj.default : obj;
 }
 
-
-function Loadable(loaderConfig) {
-
+function Loadable(loadableConfig) {
     let res = null;
+    if(!loadableConfig.loader || !loadableConfig.path) {
+        throw Error('params: loader and path required!');
+    }
+
+    function tryGetInitProps(component, match, resolve, reject, passthrough = {}) {
+        if(!component) {
+            return reject('component not exist!');
+        }
+        if(typeof component.getInitialProps === 'function' && typeof initProps === 'undefined') {
+            console.log("invok component.getInitialProps....")
+            component.getInitialProps({ match }).then(initProps => {
+                res = Object.assign({
+                    component,
+                    initProps,
+                    ready: true,
+                }, passthrough);
+                resolve(res);
+            }, () => {
+                reject('invok getInitialProps fail!')
+            })
+        }
+        else {
+            res = Object.assign({
+                component,
+                ready: true,
+            }, passthrough);
+            resolve(res);
+        }
+    }
 
     const init = (match, initProps) => {
         return new Promise((resolve, reject) => {
-            if(res) {
-                return resolve(res);
-            }
-            if(!res) {
-                res = {
-                    loaderRes: loaderConfig.loader._importModule(),
-                }
-            }
-            if(!(res.loaderRes instanceof Promise)) {
-                res.loaderRes = Promise.resolve(res.loaderRes);
+            const { loader } = loadableConfig;
+            let component = null;
 
-            }
-            res.loaderRes.then(m => {
-                res.component = resolveModule(m);
-                if(typeof res.component.getInitialProps === 'function' && typeof initProps === 'undefined') {
-                    console.log("invok initProps ajax....")
-                    return res.component.getInitialProps({ match });
+            if(typeof loader.isReady === 'function'
+               && typeof loader.chunkName === 'function'
+               && typeof loader.requireAsync === 'function'
+               && typeof loader.requireSync === 'function'
+            ) {
+                let chunkName = loader.chunkName();
+                const passthrough = {chunkName};
+
+                if(!loader.isReady()) {
+                    loader.requireAsync().then(r => {
+                        component = resolveModule(r);
+                        tryGetInitProps(component, match, resolve, reject, passthrough);
+                    }, () => {
+                        reject('requireAsync component fail!')
+                    })
                 }
-                return initProps;
-            })
-            .then(initProps => {
-                res.initProps = initProps;
-                res.requestModuleId = loaderConfig.loader._requestModuleId();
-                res.requestText = loaderConfig.loader._requestText;
-                res.ready = true;
-                resolve(res);
-            })
-            .catch(error => {
-                console.error(error);
-                resolve(res);
-            })
+                else {
+                    component = resolveModule(loader.requireSync());
+                    tryGetInitProps(component, match, resolve, reject, passthrough);
+                }
+            }
+            else if(typeof loader === 'function') {
+                loader().then(r => {
+                    component = resolveModule(r);
+                    tryGetInitProps(component, match, resolve, reject);
+                }, () => {
+                    reject('requireAsync component fail!')
+                })
+            }
+            else {
+                reject('loader must be a func, or  obj enhanced by babel plugin!')
+            }
         })
     }
     PAGE_LOADERS.push({
-        loaderConfig,
+        loadableConfig,
         init,
     });
 
     return class LoadableComponent extends React.Component {
         constructor(props) {
             super(props);
-            this.initRes = init();
         }
         componentDidMount() {
-            if(!res) {
-                throw Error('no init Loadable!');
-            }
-            if(!res.ready && this.initRes) {
-                this.initRes.then(() => {
-                    console.log('....async LoadableComponent update....')
-                    this.forceUpdate();
-                })
-            }
+            init().then(() => {
+                console.log('....async LoadableComponent update....')
+                this.forceUpdate();
+            }, (err) => {
+                console.error('init component err', err);
+                this.err = err;
+            })
         }
         render() {
-            console.log('.................render LoadableComponent...............')
-            if(res && res.ready && res.component) {
+            console.log('==========loadable render==========', res)
+            if(res && res.ready) {
                 const { component: Component, initProps } = res;
 
-                return loaderConfig.render ? loaderConfig.render({
+                return loadableConfig.render ? loadableConfig.render({
                     Component,
                     initProps,
                     props: this.props,
                 }) : React.createElement(Component, { initProps, ...this.props })
             }
-            return null;
+            if(this.err && loadableConfig.error) {
+                return React.createElement(loadableConfig.error, {err: this.err});
+            }
+            return loadableConfig.loadding ? React.createElement(loadableConfig.loadding) : null;
         }
     }
 }
 
-Loadable.loadReady = function (url, initProps) {
-    console.log('=================loadReady entry=============');
+Loadable.loadReady = function (url, initProps, matchPathFunc = matchPath) {
     let plSize = PAGE_LOADERS.length;
     let loaderConfig = null;
     let init = null;
     let match = null;
 
-    console.log('loadReady: page total: ', plSize);
-
-    //while(plSize--) {
-        //const pageLoader = PAGE_LOADERS.pop();
-        //loaderConfig = pageLoader.loaderConfig;
-        //init = pageLoader.init;
-
-        //const match = matchPath(url, {
-            //path: loaderConfig.path
-        //})
-
-        //if(match) {
-            //console.log('loadReady: match success!');
-            //break;
-        //}
-    //}
     for(let i = 0; i < plSize; i++) {
         const pageLoader = PAGE_LOADERS[i];
-        loaderConfig = pageLoader.loaderConfig;
+        const loadableConfig = pageLoader.loadableConfig;
         init = pageLoader.init;
 
-        const match = matchPath(url, {
-            path: loaderConfig.path
+        match = matchPathFunc(url, {
+            path: loadableConfig.path
         })
 
         if(match) {
-            console.log('loadReady: match success!');
+            console.log('loadReady: match success:', loadableConfig.path);
             break;
         }
     }
 
-    if(typeof init === 'function') {
+    if(typeof init === 'function' && match) {
         return init(match, initProps).then(res => {
             console.log('loadReady: match result: \n path:', url, ', router:', res);
             res.path = url;
             return res;
         })
     }
-
     return Promise.reject('no macth');
 }
-
-//Loadable.loadReady = function () {
-    //let plSize = PAGE_LOADERS.length;
-    //const allPromises = [];
-
-    //console.log('page loaders total: ', plSize);
-
-    //while(plSize--) {
-        //const { loaderConfig, init } = PAGE_LOADERS.pop();
-        //// 获得moduleId
-        //let moduleId = null;
-        //if(typeof loaderConfig.loader._pp === 'undefined') continue;
-        //moduleId = loaderConfig.loader._pp();
-        //if(!moduleId) continue;
-
-        //// 判断module是否已经加载
-        //console.log(moduleId, ' isModuleLoaded: ', isModuleLoaded(moduleId));
-        //if(!isModuleLoaded(moduleId)) continue;
-
-        //allPromises.push(init());
-    //}
-
-    //console.log('page loaders loadReady count:', allPromises.length);
-
-    //if(allPromises.length > 0) {
-        //return Promise.all(allPromises)
-    //}
-
-    //return Promise.resolve();
-//}
 
 export default Loadable;
 
